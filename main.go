6f08b96a6185070a3602f15a0814688bfca56e63
wanfintara <wanfintara@gmail.com>
2019-10-26 00:05:09 +0700

    checkout internal validation

--- main.go
+++ main.go
@@ -5,13 +5,13 @@ import (
 	fmt "fmt"
 	"os"
 
-	pb "github.com/wanfintara/grpc-tkp/protos"
+	pb "github.com/wanfintara/grpc-tkp/pb"
 	grpc "google.golang.org/grpc"
 )
 
 func main() {
-	conn, err := grpc.Dial("10.255.13.244:9000", grpc.WithInsecure())
-	// conn, err := grpc.Dial("10.255.13.244:9000")
+	// conn, err := grpc.Dial("10.255.13.244:9000", grpc.WithInsecure())
+	conn, err := grpc.Dial(":9900", grpc.WithInsecure())
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "could not connect to backend: %v\n", err)
 		os.Exit(1)
@@ -22,16 +22,16 @@ func main() {
 	// a, err := client.GetCategories(ctx, &pb.GetCategoriesRequest{})
 
 	// * CreateOrder
-	data := pb.CreateOrderRequest{
-		TxInvoice: "invoice",
-		TxAmount:  999.789,
-		TxStatus:  18,
-		OrderDetail: &pb.OrderDetailRequest{
-			VendorId:  41,
-			ProductId: 99,
-		},
-	}
-	a, err := client.CreateOrder(ctx, &data)
+	// data := pb.CreateOrderRequest{
+	// 	TxInvoice: "invoice",
+	// 	TxAmount:  999.789,
+	// 	TxStatus:  18,
+	// 	OrderDetail: &pb.OrderDetailRequest{
+	// 		VendorId:  41,
+	// 		ProductId: 99,
+	// 	},
+	// }
+	// a, err := client.CreateOrder(ctx, &data)
 
 	// // *GetOrder
 	// a, err := client.GetOrder(ctx, &pb.GetOrderRequest{OrderId: 1})
@@ -43,6 +43,34 @@ func main() {
 	// })
 
 	// a, err := client.GetProducts(ctx, &pb.GetProductsRequest{Limit: 1})
+
+	// * Checkout Validation
+
+	// a := 212121
+	// b := &any.Any{
+	// 	Value: []byte("tokopedia localservices"),
+	// 	// Value: []byte{b},
+	// },
+	// 	c[""]
+
+	// data := pb.CheckoutValidationRequest{
+	// 	BusinessType: "LOCALSERVICEs",
+	// 	Metadata:     "this is string json, dont put anything else",
+	// 	CartInfo: []*pb.CartInfo{
+	// 		{
+	// 			Quantity: 1,
+	// 			DataType: "product",
+	// 			RequestField: map[string]*any.Any{
+	// 				"test": &any.Any{
+	// 					Value: []byte("tokopedia localservices"),
+	// 					// Value: []byte{b},
+	// 				},
+	// 			},
+	// 		},
+	// 	},
+	// }
+
+	// a, err := client.CheckoutValidation(ctx, &data)
 
 	if err != nil {
 		fmt.Println(err)
